3244dd54edb4969af66c867bddceb2180da62021
Jonas Devries <jonas@good-it.be>
2017-01-03 16:19:09 +0100

    changes of the day: my first website since a long time, there is also the template where it's based on

---
+++ src/main/resources/public/linux-lessons-learned/js/app.js
@@ -0,0 +1,34 @@
+// Backgrounds.
+var backgrounds = [1,2,3,4,5,6]
+
+// Highlighting.
+hljs.initHighlighting()
+
+// Storage.
+var store = window.sessionStorage
+
+// Background.
+var background = store.getItem('background')
+
+if (!background) {
+  var i = Math.random() * backgrounds.length | 0
+  background = backgrounds[i]
+  console.log('setting background to %s', background)
+  store.setItem('background', background)
+}
+
+// Body class for background.
+var el = document.getElementById('header-overlay')
+el.style.backgroundImage = 'url(public/images/'+background+'.jpg)'
+
+// Attach class to #menu element depending on page offset.
+document.addEventListener("DOMContentLoaded", chooseMenuColor)
+window.onscroll = chooseMenuColor
+
+function chooseMenuColor() {
+  var menuElement = document.getElementById('menu')
+  var menuTopOffset = 45
+  window.innerHeight < window.pageYOffset + menuTopOffset ?
+    menuElement.classList.remove('over-header') :
+    menuElement.classList.add('over-header')
+}
