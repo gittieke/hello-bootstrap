b9f41605de931c7cb515ca1a0bfdda39dd9cd50d
Jonas Devries <jonas@good-it.be>
2017-01-13 16:35:00 +0100

    add print context beans

--- src/main/java/be/jslm/App.java
+++ src/main/java/be/jslm/App.java
@@ -1,7 +1,10 @@
 package be.jslm;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Hello world!
@@ -12,6 +15,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class, args);
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+		    System.out.println(beanName);
+		}        
+        
     }
 }
